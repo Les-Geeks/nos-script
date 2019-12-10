ff55275f06164829e522b40ffebf8d49a49226cc
d3v-donkey <d3v-donkey@buster.lan>
2019-12-10 14:51:08 +0100

    test script

---
+++ script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#==============================================================================================================
+#
+# Auteur  : Alexandre Maury
+# License : Distributed under the terms of GNU GPL version 2 or later
+#
+# GitHub : https://github.com/d3v-donkey
+#==============================================================================================================
+
+cd Downloads && git clone https://github.com/adi1090x/slim_themes.git 
+
+cd slim_themes/themes 
+
+
+
