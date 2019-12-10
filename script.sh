e7f27d6b50c04b05c417a09cada8e9e7e5215d16
d3v-donkey <d3v-donkey@buster.lan>
2019-12-10 15:12:33 +0100

    update

--- script.sh
+++ script.sh
@@ -12,7 +12,9 @@ cd
 
 cd ~/Downloads/ && git clone https://github.com/adi1090x/slim_themes.git 
 
-cd slim_themes/themes 
+cd slim_themes/themes && sudo mv overlay /usr/share/slim/themes/
+
+sudo sed -i -r 's/.*current_theme.*/current_theme overlay/g' /etc/slim.conf
 
 
 
