6fc9a3938737324d2d3047e86b5df575155edbc9
d3v-donkey <d3v-donkey@buster.lan>
2019-12-10 16:23:47 +0100

    update

--- script.sh
+++ script.sh
@@ -8,49 +8,59 @@
 # GitHub : https://github.com/d3v-donkey
 #==============================================================================================================
 
+theme=""
+
 
 cd 
 
-theme=""
-string_length=${#theme}
+function new_theme () {
+    lenghtChaine=${#theme}
+    # opérateurs -gt, -ge, -eq, -ne, -lt ou -le (pour >, >=, =, !=, <= ou <)
+    if [[ $lenghtChaine -lt 1 ]]; then
 
-# opérateurs -gt, -ge, -eq, -ne, -lt ou -le (pour >, >=, =, !=, <= ou <)
-if [[ $string_length -lt 0 ]]; then
+        echo 'test 2'
 
-    PS3="Choix du theme : "
+        cd ~/Downloads/ && git clone https://github.com/adi1090x/slim_themes.git 
 
-    select item in "- darky_pink -" "- greeny_dark -" "- -" "- Exit -"
-    do
-        echo "Vous avez choisi le theme $REPLY : $item"
-        case $REPLY in
-                1)
+        cd slim_themes/themes && sudo mv $theme /usr/share/slim/themes/
+
+        sudo sed -i -r "s/.*current_theme.*/current_theme ${theme}/g" /etc/slim.conf
+
+        echo 'Installation terminée redemarer le pc !'
+    fi
+
+}
+
+
+########## Main ##########
+
+PS3="Choix du theme : "
+
+select item in "- darky_pink -" "- greeny_dark -" "- -" "- Exit -"
+do
+    echo "Vous avez choisi le theme $REPLY : $item"
+    case $REPLY in
+            1)
                     
-                    theme="darky_pink"
-                    ;;
-                2)
+                theme="darky_pink"
+                new_theme
+                exit 0
+                ;;
+            2)
                         
-                    theme="greeny_dark"
-                    ;;
-                3)
-                    echo "Fin du script"
-                    exit 0
-                    ;;
-                *)
-                    echo "Choix incorrect"
-                    ;;
-            esac
-    done
-
-
-    cd ~/Downloads/ && git clone https://github.com/adi1090x/slim_themes.git 
-
-    cd slim_themes/themes && sudo mv $theme /usr/share/slim/themes/
-
-    sudo sed -i -r "s/.*current_theme.*/current_theme ${theme}/g" /etc/slim.conf
-fi
-
-
-
+                theme="greeny_dark"
+                new_theme
+                exit 0
+                ;;
+            3)
+                echo "Fin du script"
+                exit 0
+                ;;
+            *)
+                echo "Choix incorrect"
+                ;;
+        esac
+done
 
 
 
