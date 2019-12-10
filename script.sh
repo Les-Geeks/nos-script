a2f79e9d55d9bced0292786c2ba9377bef916f96
d3v-donkey <d3v-donkey@buster.lan>
2019-12-10 15:46:50 +0100

    update

--- script.sh
+++ script.sh
@@ -8,13 +8,49 @@
 # GitHub : https://github.com/d3v-donkey
 #==============================================================================================================
 
+
 cd 
 
-cd ~/Downloads/ && git clone https://github.com/adi1090x/slim_themes.git 
+theme = ''
+longueur=${#theme}
 
-cd slim_themes/themes && sudo mv overlay /usr/share/slim/themes/
+if ($longueur < 0); then
 
-sudo sed -i -r 's/.*current_theme.*/current_theme overlay/g' /etc/slim.conf
+    PS3="Choix du theme : "
+
+    select item in "- darky_pink -" "- greeny_dark -" "- -" "- Exit -"
+    do
+        echo "Vous avez choisi le theme $REPLY : $item"
+        case $REPLY in
+                1)
+                    
+                    theme="darky_pink"
+                    ;;
+                2)
+                        
+                    theme="greeny_dark"
+                    ;;
+                3)
+                    echo "Fin du script"
+                    exit 0
+                    ;;
+                *)
+                    echo "Choix incorrect"
+                    ;;
+            esac
+    done
+
+
+    cd ~/Downloads/ && git clone https://github.com/adi1090x/slim_themes.git 
+
+    cd slim_themes/themes && sudo mv $theme /usr/share/slim/themes/
+
+    sudo sed -i -r 's/.*current_theme.*/current_theme ${theme}/g' /etc/slim.conf
+fi
+
+
+
+
 
 
 
